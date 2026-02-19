9c45140d68be30ed73d073db2a0bddcf6c6ac393
Robert Chiniquy <rchiniquy@yahoo.com>
2026-02-18 16:09:43 -0800

    Add ExportTargetCLIOnly to base-url field
    Ref: https://github.com/ConductorOne/baton-prismhr/pull/13#discussion_r2790908601

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -26,6 +26,7 @@ var (
 		"base-url",
 		field.WithDescription("Override the SentinelOne API URL (for testing)"),
 		field.WithHidden(true),
+		field.WithExportTarget(field.ExportTargetCLIOnly),
 	)
 )
 
