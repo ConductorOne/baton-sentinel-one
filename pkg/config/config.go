2656436f7ba642130fed6c1a0e55f895cf3df2c9
Alejandro Bernal <jbernalxyz@gmail.com>
2026-03-18 13:38:23 -0500

    containerize sentinel-one

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -7,20 +7,29 @@ import (
 var (
 	APIToken = field.StringField(
 		"api-token",
+		field.WithDisplayName("API token"),
 		field.WithDescription("API token for your management console used to authenticate with SentinelOne API."),
+		field.WithPlaceholder("Your SentinelOne API token"),
 		field.WithRequired(true),
 		field.WithIsSecret(true),
 	)
 
 	ManagementConsoleURL = field.StringField(
 		"management-console-url",
+		field.WithDisplayName("SentinelOne base URL"),
 		field.WithDescription("Your management console URL."),
+		field.WithPlaceholder("Your SentinelOne base URL"),
 		field.WithRequired(true),
 	)
 )
 
 //go:generate go run ./gen
-var Config = field.NewConfiguration([]field.SchemaField{
-	APIToken,
-	ManagementConsoleURL,
-})
+var Config = field.NewConfiguration(
+	[]field.SchemaField{
+		APIToken,
+		ManagementConsoleURL,
+	},
+	field.WithConnectorDisplayName("SentinelOne"),
+	field.WithHelpUrl("/docs/baton/sentinelone"),
+	field.WithIconUrl("/static/app-icons/sentinelone.svg"),
+)
